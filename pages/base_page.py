27c75e11e96898f0ed41dfd477e26a59e74c4104
Dasha Ternovskaya <dashaternovskaya@gmail.com>
2021-12-07 16:07:30 +0300

    add class MainPage, 1 method for BasePage, 1 test

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,11 +1,20 @@
 # Базовая страница, от которой будут унаследованы все остальные классы.
 # В ней мы описаны вспомогательные методы для работы с драйвером.
+from selenium.common.exceptions import NoSuchElementException
 
 
-class BasePage():
-    def __init__(self, browser, url):
+class BasePage:
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
