a8e4291b58433c2c106fbe7ac91506fd29114cc9
Dasha Ternovskaya <dashaternovskaya@gmail.com>
2021-12-08 18:34:53 +0300

    add new Page Object login_page.py, test_login_page.py, locators.py
    upd. test_main_page.py (add LoginPage-test)

---
+++ pages/login_page.py
@@ -0,0 +1,21 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # проверка на корректный url адрес
+        assert "login" in self.browser.current_url, "Login link is not presented"  # или self.browser.current_url.index("login")
+
+    def should_be_login_form(self):
+        # проверка, что есть форма логина на странице
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
+    def should_be_register_form(self):
+        # проверка, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), "Registation form is not presented"
---
+++ test_login_page.py
@@ -0,0 +1,8 @@
+from pages.login_page import LoginPage
+
+
+def test_should_be_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
