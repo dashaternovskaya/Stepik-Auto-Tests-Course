0b5ad1913b67b5432e13fa3d75ecde05c6fbf5f9
Dasha Ternovskaya <dashaternovskaya@gmail.com>
2021-12-10 18:34:19 +0300

    upd. locators.py (add class ProductPageLocators)
    add 1 method for BasePage

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,11 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators:
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, "button[value='Add to basket']")  # или ADD_TO_BASKET_BUTTON = (By.XPATH, "//button[@value='Add to basket']")
+    PAGE_PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main > h1")
+    SUCCESS_MESSAGE_PRODUCT_NAME = (By.CSS_SELECTOR, ".alert:nth-child(1) strong")
+    PAGE_PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main > p.price_color")
+    SUCCESS_MESSAGE_PRODUCT_PRICE = (By.CSS_SELECTOR, ".alert:nth-child(3) strong")
